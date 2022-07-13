3d077a74bb4c324a6a9f50cbe956766a4b2a338a
Vasilievkosta <vasilievkosta@gmail.com>
2022-07-13 22:20:39 +0300

    added blog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function () {
 
+    $('.specific__link').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('.specific__link').removeClass('specific__link--active');
+        $(this).addClass('specific__link--active');
+
+        $('.specific__item').removeClass('specific__item--active');
+        $($(this).attr('href')).addClass('specific__item--active');
+    });
+
     $('.select-styler').styler();
 
     $('.specific__number').styler();
@@ -56,27 +67,56 @@ $(function () {
         slidesToShow: 5,
         slidesToScroll: 3,
         responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
             }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
         ]
+    });
+
+    $('.related__list').slick({
+        infinite: true,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        // responsive: [{
+        //         breakpoint: 1439,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 1199,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 575,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
     });
 
     $('.specific__small').slick({
@@ -86,7 +126,6 @@ $(function () {
         slidesToScroll: 1,
         vertical: true,
         draggable: false
-
 
     });
 
@@ -102,13 +141,22 @@ $(function () {
 
 
     $(function () {
+        $(".specific__star").rateYo({
+            starWidth: "18px",
+            spacing: "12px",
+            normalFill: "#d6d6d6",
+            ratedFill: "#ffcc00",
+            readOnly: true,
+            starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="16px" viewBox="0 0 18 16" version="1.1"><path d="M 11.914062 4.695312 L 16.402344 5.359375 C 16.773438 5.414062 17.085938 5.675781 17.207031 6.035156 C 17.324219 6.398438 17.226562 6.789062 16.960938 7.058594 L 13.703125 10.253906 L 14.472656 14.835938 C 14.535156 15.210938 14.382812 15.589844 14.070312 15.8125 C 13.757812 16.035156 13.351562 16.0625 13.015625 15.882812 L 9.003906 13.742188 L 4.992188 15.882812 C 4.65625 16.0625 4.246094 16.035156 3.9375 15.8125 C 3.628906 15.589844 3.472656 15.210938 3.539062 14.835938 L 4.304688 10.253906 L 1.050781 7.058594 C 0.78125 6.789062 0.683594 6.398438 0.804688 6.035156 C 0.921875 5.675781 1.230469 5.414062 1.605469 5.359375 L 6.09375 4.695312 L 8.105469 0.5625 C 8.273438 0.21875 8.621094 0 9.003906 0 C 9.386719 0 9.738281 0.21875 9.902344 0.5625 Z M 11.914062 4.695312 " /></svg>'
+        });
 
         $(".catalog__star").rateYo({
             starWidth: "10px",
             spacing: "6px",
             normalFill: "#d6d6d6",
             ratedFill: "#ffcc00",
-            readOnly: true
+            readOnly: true,
+            starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="16px" viewBox="0 0 18 16" version="1.1"><path d="M 11.914062 4.695312 L 16.402344 5.359375 C 16.773438 5.414062 17.085938 5.675781 17.207031 6.035156 C 17.324219 6.398438 17.226562 6.789062 16.960938 7.058594 L 13.703125 10.253906 L 14.472656 14.835938 C 14.535156 15.210938 14.382812 15.589844 14.070312 15.8125 C 13.757812 16.035156 13.351562 16.0625 13.015625 15.882812 L 9.003906 13.742188 L 4.992188 15.882812 C 4.65625 16.0625 4.246094 16.035156 3.9375 15.8125 C 3.628906 15.589844 3.472656 15.210938 3.539062 14.835938 L 4.304688 10.253906 L 1.050781 7.058594 C 0.78125 6.789062 0.683594 6.398438 0.804688 6.035156 C 0.921875 5.675781 1.230469 5.414062 1.605469 5.359375 L 6.09375 4.695312 L 8.105469 0.5625 C 8.273438 0.21875 8.621094 0 9.003906 0 C 9.386719 0 9.738281 0.21875 9.902344 0.5625 Z M 11.914062 4.695312 " /></svg>'
         });
 
         $(".product-card__star").rateYo({
@@ -116,16 +164,10 @@ $(function () {
             spacing: "12px",
             normalFill: "#d6d6d6",
             ratedFill: "#ffcc00",
-            readOnly: true
+            readOnly: true,
+            starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="16px" viewBox="0 0 18 16" version="1.1"><path d="M 11.914062 4.695312 L 16.402344 5.359375 C 16.773438 5.414062 17.085938 5.675781 17.207031 6.035156 C 17.324219 6.398438 17.226562 6.789062 16.960938 7.058594 L 13.703125 10.253906 L 14.472656 14.835938 C 14.535156 15.210938 14.382812 15.589844 14.070312 15.8125 C 13.757812 16.035156 13.351562 16.0625 13.015625 15.882812 L 9.003906 13.742188 L 4.992188 15.882812 C 4.65625 16.0625 4.246094 16.035156 3.9375 15.8125 C 3.628906 15.589844 3.472656 15.210938 3.539062 14.835938 L 4.304688 10.253906 L 1.050781 7.058594 C 0.78125 6.789062 0.683594 6.398438 0.804688 6.035156 C 0.921875 5.675781 1.230469 5.414062 1.605469 5.359375 L 6.09375 4.695312 L 8.105469 0.5625 C 8.273438 0.21875 8.621094 0 9.003906 0 C 9.386719 0 9.738281 0.21875 9.902344 0.5625 Z M 11.914062 4.695312 " /></svg>'
         });
 
-        $(".specific__star").rateYo({
-            starWidth: "18px",
-            spacing: "12px",
-            normalFill: "#d6d6d6",
-            ratedFill: "#ffcc00",
-            readOnly: true
-        });
 
     });
 
