37993c845cb31c66811fd3c9b092b8b4876c0521
Vasilievkosta <vasilievkosta@gmail.com>
2023-05-16 15:17:26 +0300

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
-const {
-    src,
-    dest,
-    watch,
-    parallel,
-    series
-} = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
+
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify-es').default;
-
-// const imagemin = require('gulp-imagemin');
-
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
 const svgSprite = require('gulp-svg-sprite');
 const fileInclude = require('gulp-file-include');
 
@@ -26,13 +25,30 @@ function browsersync() {
     })
 }
 
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
 function styles() {
     return src('app/scss/style.scss')
-		.pipe(autoprefixer({
+        .pipe(autoprefixer({
             overrideBrowserslist: ['last 10 versions'],
             grid: true
         }))
-		.pipe(concat('style.min.css'))
+        .pipe(concat('style.min.css'))
         .pipe(scss({
             outputStyle: 'expanded' // outputStyle: 'compressed'
         }))
@@ -40,51 +56,53 @@ function styles() {
         .pipe(browserSync.stream())
 }
 
-function svgSprites() {
+function watching() {
+    watch(['app/**/*.scss'], styles);
+    watch(['app/images/**/*.*'], images);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch('app/html/**/*.html', fileincludes);
+    watch(['app/**/*.html']).on('change', browserSync.reload);
+
+    // watch(['app/images/**/*.svg'], sprites);
+}
+
+function sprites() {
     return src('app/images/**/*.svg')
         .pipe(svgSprite({
             mode: {
                 stack: {
-                    sprite: 'sprite.svg'
+                    sprite: 'sprite.svg',
+                    // example: true
                 }
             }
         }))
         .pipe(dest('app/images'))
 }
 
-function scripts() {
-    return src([
-            'node_modules/jquery/dist/jquery.js',
-            'node_modules/slick-carousel/slick/slick.js',
-            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-            'node_modules/mixitup/dist/mixitup.js',
-            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-            'node_modules/rateyo/src/jquery.rateyo.js',
-            'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-            'app/js/main.js'
-        ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+function fonts() {
+    return src('app/fonts/src/*.*') // sources should be in src        
+        .pipe(fonter({
+            formats: ['woff', 'ttf']
+        }))
+
+        .pipe(src('app/fonts/*.ttf'))
+        .pipe(ttf2woff2())
+        .pipe(dest('app/fonts'))
 }
 
-// function images() {
-    // return src('app/images/**/*.*')
-        // .pipe(imagemin({
-            // interlaced: true,
-            // progressive: true,
-            // optimizationLevel: 5,
-            // svgoPlugins: [{
-                // removeViewBox: true
-            // }]
-        // }))
-        // .pipe(dest('dist/images'))
-// }
-
 function images() {
-    return src('app/images/**/*.*')
-        // .pipe(imagemin())
+    return src(['app/images/**/*.*', '!app/images/**/*.svg'])
+        // .pipe(newer('dist/images'))
+        // .pipe(avif({ quality: 50 }))
+
+        // .pipe(src('app/images/**/*.*'))
+        // .pipe(newer('dist/images'))
+        // .pipe(webp())
+
+        .pipe(src('app/images/**/*.*'))
+        .pipe(newer('dist/images'))
+        .pipe(imagemin())
+
         .pipe(dest('dist/images'))
 }
 
@@ -97,43 +115,37 @@ function fileincludes() {
 
 function building() {
     return src([
-            'app/**/*.html',
-            'app/css/style.min.css',
-            'app/js/main.min.js'
-        ], {
-            base: 'app'
-        })
+        'app/*.html',
+        'app/css/style.min.css',
+        'app/js/main.min.js',
+        'app/fonts/*.*'
+    ], {
+        base: 'app'
+    })
         .pipe(dest('dist'))
 }
 
 function cleanDist() {
     return src('dist')
-		pipe(clean())
+        .pipe(clean())
 }
 
-function watching() {
-    watch(['app/**/*.scss'], styles);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch('app/html/**/*.html', fileincludes);
-    watch(['app/**/*.html']).on('change', browserSync.reload);
 
-    watch(['app/images/**/*.svg'], svgSprites);
-
-}
 
 exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.fonts = fonts;
 exports.cleanDist = cleanDist;
+exports.building = building;
 
 exports.build = series(cleanDist, images, building);
-// exports.build = series(cleanDist, build);
+// exports.build = series(cleanDist, building);
 
 exports.fileincludes = fileincludes;
-exports.svgSprites = svgSprites;
+exports.sprites = sprites;
 
-// exports.default = parallel(styles, scripts, svgSprites, browsersync, watching);
 // exports.default = parallel(styles, scripts, browsersync, watching);
 exports.default = parallel(fileincludes, styles, scripts, browsersync, watching);
